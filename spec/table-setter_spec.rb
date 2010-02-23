0ca74593c380444a68cb13de4520308dda81c1ae
Jeff Larson <thejefflarson@gmail.com>
2010-02-22 19:29:39 -0500

    template installation and quick serving

--- spec/table-setter_spec.rb
+++ spec/table-setter_spec.rb
@@ -1,7 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "TableSetter" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe TableSetter::Table do
+  before :all do
+    @table = TableSetter::Table.new("test")
+  end
+  
+  it "should load from a google key" do
+    @table.data.should_not be_nil
+    @table.data.headers.should_not be_nil
+  end
+  
+  it "should load from a google key" do
+    @table.data.should_not be_nil
+  end
+  
+  it "should report the style for a header" do
+    @table.data.rows[0].column_for('Bank').style.should eql 'text-align:left;'
   end
 end
