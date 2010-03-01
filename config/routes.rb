5ca95cde1b8f4670ae9309f14127808638f1746b
Jeff Larson <thejefflarson@gmail.com>
2010-03-01 16:03:58 -0500

    first step of merge

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.table ':slug/:page', :controller => 'table', :action => 'view', :requirements => { :page => /\d+/}, :page => nil
-  map.connect ':slug/expire', :controller => 'table', :action => 'expire'
-  map.root :controller =>'table', :action => 'index'
-end
