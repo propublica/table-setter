56022f43a6db1aa857195acd3b23bf6538a8a791
Jeff Larson <thejefflarson@gmail.com>
2010-03-02 12:01:17 -0500

    sinatra on rails

--- app/metal/table_setter_rack.rb
+++ app/metal/table_setter_rack.rb
@@ -1,20 +1,21 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
-TableSetter.configure File.dirname(__FILE__)
+TableSetter.configure File.join(RAILS_ROOT, "app", "metal")
 
 require 'rack'
 require 'rack/showexceptions'
 require 'rack/commonlogger'
 require 'rack/lint'
 
-TableSetterRack = Rack::Builder.app do 
+TableSetterRack = Rack::Builder.app do
+  TableSetter::App.cache_timeout = 60 * 15 if Rails.env == 'development'
   use Rack::CommonLogger, STDERR
   use Rack::ShowExceptions if Rails.env == 'development'
   use Rack::Lint if Rails.env == 'development'
   use(Rack::Cache, 
     :verbose     => true,
-    :metastore   => "file:#{RAILS_ROOT}/rack/cache/meta",
-    :entitystore => "file:#{RAILS_ROOT}/rack/cache/body") if Rails.env == 'production'
+    :metastore   => "file:#{RAILS_ROOT}/tmp/cache/rack/meta",
+    :entitystore => "file:#{RAILS_ROOT}/tmp/cache/rack/body") if Rails.env == 'production'
   map "/" do
     run TableSetter::App
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,3 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-TableSetter::App.cache_timeout = 60 * 15 
