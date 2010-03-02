ad71ffdb1ae7ec0321fd2b52f4b4b4876f741a3f
Jeff Larson <thejefflarson@gmail.com>
2010-03-02 11:46:05 -0500

    stubbing out the rails branch but there's a booboo in the gem

--- app/metal/table_setter_rack.rb
+++ app/metal/table_setter_rack.rb
@@ -2,6 +2,20 @@
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
 TableSetter.configure File.dirname(__FILE__)
 
+require 'rack'
+require 'rack/showexceptions'
+require 'rack/commonlogger'
+require 'rack/lint'
 
-class TableSetterRack < TableSetter::App
+TableSetterRack = Rack::Builder.app do 
+  use Rack::CommonLogger, STDERR
+  use Rack::ShowExceptions if Rails.env == 'development'
+  use Rack::Lint if Rails.env == 'development'
+  use(Rack::Cache, 
+    :verbose     => true,
+    :metastore   => "file:#{RAILS_ROOT}/rack/cache/meta",
+    :entitystore => "file:#{RAILS_ROOT}/rack/cache/body") if Rails.env == 'production'
+  map "/" do
+    run TableSetter::App
+  end
 end
